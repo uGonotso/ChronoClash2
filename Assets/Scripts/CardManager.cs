35b206de1918fa48332d35ff9663f95dd7dcb616
kgomotso <79301927+uGonotso@users.noreply.github.com>
2024-06-02 17:13:28 +0200

    Scoring Mechanism Created
    The number of matches and turns left can now be tracked and game states like level won or level lost can now be determined.

--- Assets/Scripts/CardManager.cs
+++ Assets/Scripts/CardManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CardManager : MonoBehaviour
 {
@@ -10,15 +11,28 @@ public class CardManager : MonoBehaviour
     // List of card positions
     public List<Transform> cardPositions;
 
+    // Text objects to display the number of matches and turns remaining
+    public Text matchesText, turnsText;
+
+    // Number of matches required to win and turns left to play
+    public int matchesRequired, turnsLeft;
+
     // List to store the instantiated cards
     private List<Card> cards = new List<Card>();
 
     // Cards that are currently flipped up
     private List<Card> flippedCards = new List<Card>();
 
+    // Number of matches made
+    private int matchesMade = 0;
+
     // Start is called before the first frame update
     void Start()
     {
+        // Update the text objects
+        matchesText.text = "Matches: " + matchesMade;
+        turnsText.text = "Turns Left: " + turnsLeft;
+
         // Create a temporary list of card prefabs
         List<GameObject> tempPrefabs = new List<GameObject>(cardPrefabs);
 
@@ -63,7 +77,7 @@ public class CardManager : MonoBehaviour
     {
         flippedCards.Add(card);
 
-        if (flippedCards.Count >= 2)
+        if (flippedCards.Count == 2)
         {
             StartCoroutine(CheckMatch());
         }
@@ -74,27 +88,44 @@ public class CardManager : MonoBehaviour
         // Wait for 1 second
         yield return new WaitForSeconds(1f);
 
-        while (flippedCards.Count >= 2)
+        Card card1 = flippedCards[0];
+        Card card2 = flippedCards[1];
+
+        if (card1.faceSprite == card2.faceSprite)
         {
-            Card card1 = flippedCards[0];
-            Card card2 = flippedCards[1];
+            // It's a match! Destroy both cards
+            Destroy(card1.gameObject);
+            Destroy(card2.gameObject);
 
-            if (card1.faceSprite == card2.faceSprite)
-            {
-                // It's a match! Destroy both cards
-                Destroy(card1.gameObject);
-                Destroy(card2.gameObject);
-            }
-            else
-            {
-                // Not a match, flip both cards back down
-                card1.FlipCardDown();
-                card2.FlipCardDown();
-            }
+            // Increment the number of matches made
+            matchesMade++;
+            matchesText.text = "Matches: " + matchesMade;
 
-            // Remove the cards from the flippedCards list
-            flippedCards.Remove(card1);
-            flippedCards.Remove(card2);
+            // Check if the required number of matches has been made
+            if (matchesMade == matchesRequired)
+            {
+                Debug.Log("Level won!");
+            }
         }
+        else
+        {
+            // Not a match, flip both cards back down
+            card1.FlipCardDown();
+            card2.FlipCardDown();
+        }
+
+        // Decrement the number of turns left
+        turnsLeft--;
+        turnsText.text = "Turns Left: " + turnsLeft;
+
+        // Check if there are no more turns left
+        if (turnsLeft == 0)
+        {
+            Debug.Log("Level lost!");
+        }
+
+        // Remove the cards from the flippedCards list
+        flippedCards.Remove(card1);
+        flippedCards.Remove(card2);
     }
 }
